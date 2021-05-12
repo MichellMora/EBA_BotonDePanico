5911b76afdf4cf1ca78d79d425d00314bcd9b4a0
MichellMora <mdmora36@ucatolica.edu.co>
2021-05-12 13:30:11 -0500

    envía mensajes al botón correspondiente con ubicación

--- app/src/main/java/Boton_de_panico/Pag_Principal.kt
+++ app/src/main/java/Boton_de_panico/Pag_Principal.kt
@@ -1,7 +1,10 @@
 package Boton_de_panico
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.telephony.SmsManager
@@ -10,36 +13,51 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.firestore.FirebaseFirestore
 import com.proyecto.ebabotndepnico.R
+import com.proyecto.ebabotndepnico.contactos
 import kotlinx.android.synthetic.main.activity_pag__principal.*
-
 
 class Pag_Principal : AppCompatActivity() {
 
     private val bd = FirebaseFirestore.getInstance()
+    private val code =  11
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pag__principal)
 
-        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)!= PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.SEND_SMS),111)
-        }
-
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        solicitarPermisos()
         enviarSMS()
+        //ubicacion()
+    }
+
+    private fun solicitarPermisos(){
+        val SMS = ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+        val Localizador = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if(SMS!=PackageManager.PERMISSION_GRANTED && Localizador!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.SEND_SMS,Manifest.permission.ACCESS_FINE_LOCATION),code)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode==111 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+
+        if (requestCode==code && grantResults[0]==PackageManager.PERMISSION_GRANTED){
 
             msjAprobado()
-        }
-    }
+
+        } }
+
+    // SMS
 
     @RequiresApi(Build.VERSION_CODES.R)
     private fun enviarSMS () {
@@ -64,10 +82,6 @@ class Pag_Principal : AppCompatActivity() {
                 .document(ID.toString()).collection("botones")
                 .document("RaaaBnnIUawPjsiKke0k").id
 
-        //tvDatos.setText(ID)
-        //tvDatos2.setText(correo)
-        //tvDatos3.setText(IDbtn1)
-
         val IDcont1 = bd.collection("usuarios").document(ID.toString())
                 .collection("botones").document(IDbtn1)
                 .collection("contactos").document("gnAxPhqOWTeh5TxAx9z3").id
@@ -84,51 +98,24 @@ class Pag_Principal : AppCompatActivity() {
                 .collection("botones").document(IDbtn1)
                 .collection("contactos").document("NYLVhTBV7mTYuUwkcsRE").id
 
-        btnMSJ1.setOnClickListener {
-            bd.collection("usuarios").document(ID.toString())
-                    .collection("botones").document(IDbtn1)
-                    .get().addOnSuccessListener {
+        btnMSJ1.setOnClickListener {btnMSJ(ID.toString(),IDbtn1,IDcont1,IDcont2,IDcont3,IDcont4) }
 
-                        val MSJ1 = it.get("Mensaje")
-                        msjBoton(ID.toString(),IDbtn1,IDcont1,IDcont2,IDcont3,IDcont4, MSJ1.toString())}
+        btnMSJ2.setOnClickListener { btnMSJ(ID.toString(), IDbtn2, IDcont1, IDcont2, IDcont3, IDcont4) }
 
+        btnMSJ3.setOnClickListener{ btnMSJ(ID.toString(),IDbtn3,IDcont1,IDcont2,IDcont3,IDcont4)}
 
+        btnMSJ4.setOnClickListener{ btnMSJ(ID.toString(),IDbtn4,IDcont1,IDcont2,IDcont3,IDcont4)}
 
-        }
+    }
 
-        btnMSJ2.setOnClickListener {
-            bd.collection("usuarios").document(ID.toString())
-                    .collection("botones").document(IDbtn2)
-                    .get().addOnSuccessListener {
+    private fun btnMSJ(ID : String, IDbtn: String, IDcont1: String, IDcont2 :String, IDcont3:String, IDcont4:String){
 
-                        val MSJ3 = it.get("Mensaje")
-                        msjBoton(ID.toString(), IDbtn2, IDcont1, IDcont2, IDcont3, IDcont4, MSJ3.toString())
+        bd.collection("usuarios").document(ID.toString())
+                .collection("botones").document(IDbtn)
+                .get().addOnSuccessListener {
 
-                    }
-        }
-
-        btnMSJ3.setOnClickListener{
-            bd.collection("usuarios").document(ID.toString())
-                    .collection("botones").document(IDbtn3)
-                    .get().addOnSuccessListener {
-
-                        val MSJ3 = it.get("Mensaje")
-                        msjBoton(ID.toString(),IDbtn3,IDcont1,IDcont2,IDcont3,IDcont4,MSJ3.toString())
-
-                    }
-
-        }
-
-        btnMSJ4.setOnClickListener{
-            bd.collection("usuarios").document(ID.toString())
-                    .collection("botones").document(IDbtn4)
-                    .get().addOnSuccessListener {
-
-                        val MSJ4 = it.get("Mensaje")
-                        msjBoton(ID.toString(),IDbtn4,IDcont1,IDcont2,IDcont3,IDcont4,MSJ4.toString())
-
-                    }
-
+                    val MSJ = it.get("Mensaje")
+                    msjBoton(ID.toString(),IDbtn,IDcont1,IDcont2,IDcont3,IDcont4,MSJ.toString())
         }
 
     }
@@ -154,18 +141,18 @@ class Pag_Principal : AppCompatActivity() {
 
                                 if (document.exists()) {
 
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont1)
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont2)
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont3)
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont4)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont1)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont2)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont3)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont4)
 
 
                                 } else {
 
 
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont2)
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont3)
-                                        msj(MSJ.toString(), ID.toString(),IDbtn,IDcont4)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont2)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont3)
+                                    msj(MSJ.toString(), ID.toString(),IDbtn,IDcont4)
 
 
 
@@ -176,10 +163,54 @@ class Pag_Principal : AppCompatActivity() {
 
     }
 
-
     private fun msj(MSJ:String, ID:String,IDbtn:String,IDcont:String){
 
-        bd.collection("usuarios").document(ID.toString())
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        val lat = location.latitude
+                        val lon = location.longitude
+                        Log.d("Ubicacion", "$lat  $lon");
+
+                        bd.collection("usuarios").document(ID)
+                                .collection("botones").document(IDbtn)
+                                .collection("contactos").document(IDcont).get().addOnSuccessListener { document ->
+                                    if (document.exists()) {
+                                        if (MSJ.isEmpty()){
+                                            Log.d("MSj", "El mensaje esta vacio")
+                                        }else{
+                                            var contacto = document.getString("Telefono")
+                                            var cont = SmsManager.getDefault()
+                                            cont.sendTextMessage(contacto.toString(),
+                                                    null, MSJ + "https://maps.google.com/maps?q=${lat}${lon}" , null, null)
+                                        }
+
+                                    }else {
+
+                                        Log.d("Contacto","Contacto no existe")
+
+                                    }
+
+                                }
+
+                    }else{
+                        Log.d("Ubicacion", "No encontrada");
+                    }
+                }
+
+        /*bd.collection("usuarios").document(ID.toString())
                 .collection("botones").document(IDbtn)
                 .collection("contactos").document(IDcont).get().addOnSuccessListener { document ->
                     if (document.exists()) {
@@ -189,7 +220,7 @@ class Pag_Principal : AppCompatActivity() {
                             var contacto = document.getString("Telefono")
                             var cont = SmsManager.getDefault()
                             cont.sendTextMessage(contacto.toString(),
-                                    null, MSJ, null, null)
+                                    null, MSJ , null, null)
                         }
 
                     }else {
@@ -198,13 +229,46 @@ class Pag_Principal : AppCompatActivity() {
 
                     }
 
-                }
+                }*/
 
     }
 
+    //Ubicación
+
+  /*  private fun ubicacion(){
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+            fusedLocationProviderClient.lastLocation
+                .addOnSuccessListener { location ->
+                    if (location != null) {
+                        val lat = location.latitude
+                        val lon = location.longitude
+                        Log.d("Ubicacion", "$lat  $lon");
+
+                        btnMSJ1.setOnClickListener {
+                            //val mapa = Intent(Intent.ACTION_VIEW, Uri.parse("https://maps.google.com/maps?q=${lat} ${lon}"))
+                            var cont = SmsManager.getDefault()
+                            cont.sendTextMessage("3015935096",
+                                    null, "Ubicación actual" + "https://maps.google.com/maps?q=${lat} , ${lon}", null, null)
+                            //startActivity(mapa)
+                        }
 
 
+                    }else{
+                        Log.d("Ubicacion", "No encontrada");
+                    }
+                }
 
-
+    }*/
 }
-
