dfe4daec46c2acefe074048e924c448add232ee8
MichellMora <mdmora36@ucatolica.edu.co>
2021-05-22 07:42:26 -0500

    notificacion con acceso directo a los botones de pánico

--- app/src/main/java/Boton_de_panico/Vista_Boton.kt
+++ app/src/main/java/Boton_de_panico/Vista_Boton.kt
@@ -1,20 +1,28 @@
 package Boton_de_panico
 
+import android.Manifest
 import android.app.*
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.graphics.LightingColorFilter
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import bolts.Task
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.type.LatLng
 import com.proyecto.ebabotndepnico.R
+import kotlinx.android.synthetic.main.activity_pag__principal.*
 import kotlinx.android.synthetic.main.activity_vista__boton.*
 
 class Vista_Boton : AppCompatActivity() {
@@ -23,14 +31,12 @@ class Vista_Boton : AppCompatActivity() {
     val canalNom = "canalNom"
     val notID = 0
     private val bd = FirebaseFirestore.getInstance()
+    private val code = 11
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vista__boton)
-
-        btnInicio.setOnClickListener {
-            //Boton con Widget
-        }
 
         btnNot.setOnClickListener {
             crearCanal()
@@ -45,66 +51,67 @@ class Vista_Boton : AppCompatActivity() {
         val ID = bundle?.getString("ID")
 
         bd.collection("usuarios").document(ID.toString())
-            .collection("botones").document("jRHaBP85jYEEPZjHhlEP")
-            .get().addOnSuccessListener { documento ->
+                .collection("botones").document("jRHaBP85jYEEPZjHhlEP")
+                .get().addOnSuccessListener { documento ->
 
 
-                        var msj = documento.getString("Mensaje")
-                        var Nombre = documento.getString("Nombre")
+                    var msj = documento.getString("Mensaje")
+                    var Nombre = documento.getString("Nombre")
 
-                        val intent = Intent(this, Pag_Principal::class.java).apply {
-                            putExtra("ID", ID)
-                        }
-                        val pendingIntent:PendingIntent? = TaskStackBuilder.create(this).run {
+                    val intent = Intent(this, Pag_Principal::class.java).apply {
+                        putExtra("ID", ID)
+                    }
+                    val pendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
 
-                            addNextIntentWithParentStack(intent)
-                            getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
-                        }
+                        addNextIntentWithParentStack(intent)
+                        getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+                    }
 
-                        val bi = Intent(this,Pag_Principal::class.java).apply {
-                            putExtra("ID", ID)
-                        }
-                        bi.putExtra("EXTRA_ARG", "Sí")
-                        val buttonPendingIntent = PendingIntent.getActivity(
-                                this,1,bi, PendingIntent.FLAG_ONE_SHOT
-                        )
+                    val bi = Intent(this, Pag_Principal::class.java).apply {
+                        putExtra("ID", ID)
 
-                        val action = NotificationCompat.Action.Builder(
-                                R.drawable.logo,
-                                Nombre,
-                                buttonPendingIntent
-                        ).build()
+                    }
+                    bi.putExtra("EXTRA_ARG", "Sí")
+                    val buttonPendingIntent = PendingIntent.getActivity(
+                            this, 1, bi, PendingIntent.FLAG_ONE_SHOT
+                    )
 
-                        val nm = NotificationCompat.Builder(this, canal).also {
+                    val action = NotificationCompat.Action.Builder(
+                            R.drawable.logo,
+                            "Ir",
+                            buttonPendingIntent
 
-                            it.setContentTitle("Notificación")
-                            it.setContentText(msj)
-                            it.setSmallIcon(R.drawable.logo)
-                            it.setPriority(NotificationCompat.PRIORITY_HIGH)
-                            it.setContentIntent(pendingIntent)
-                            it.addAction(action)
-                            it.setAutoCancel(false)
+                    ).build()
 
-                        }.build()
+                    val nm = NotificationCompat.Builder(this, canal).also {
 
-                        val notificationManager = NotificationManagerCompat.from(this)
+                        it.setContentTitle("Notificación")
+                        it.setContentText("Acceso directo a botones de pánico")
+                        it.setSmallIcon(R.drawable.logo)
+                        it.setPriority(NotificationCompat.PRIORITY_HIGH)
+                        it.setContentIntent(pendingIntent)
+                        it.addAction(action)
+                        it.setAutoCancel(false)
 
-                        notificationManager.notify(notID,nm)
-            }
+                    }.build()
 
+                    val notificationManager = NotificationManagerCompat.from(this)
+
+                    notificationManager.notify(notID, nm)
+                }
 
     }
 
     private fun crearCanal() {
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val importancia = NotificationManager.IMPORTANCE_HIGH
             val canal = NotificationChannel(canal, canalNom, importancia).apply {
 
                 enableLights(true)
             }
 
-            val manager:NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val manager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             manager.createNotificationChannel(canal)
 
@@ -113,9 +120,4 @@ class Vista_Boton : AppCompatActivity() {
 
     }
 
-    private fun SMS(){
-
-            Log.d("SMS", "Si llega aqui")
-
-    }
 }
